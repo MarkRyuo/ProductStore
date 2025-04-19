16c165800edcd6c78f0410c16a1a1dc4b95c68f6
MarkRyuo <malupajhonmark3035@gmail.com>
2025-04-19 12:12:21 +0800

    Subject: Update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,14 @@
+import mongoose from 'mongoose'; //* Basically going to be running to able to connect to the database by using the connection string (process.env.MONGO_URI) and the mongoose library
 
 export const connectDB = async () => { 
   try {
-
+    const mongoose = require('mongoose');
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) { 
-    
+
   }
 }
