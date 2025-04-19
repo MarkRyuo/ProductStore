ea349b6766d35f0402f4162beba9071e5abe331d
MarkRyuo <malupajhonmark3035@gmail.com>
2025-04-19 11:14:30 +0800

    Subject: Update

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
 // ! Entry point of our server
 
 import express from 'express';
+
+const app = express();
+
+app.listen(5000, (req, res) => { 
+    console.log('Server started at http://localhost:5000');
+})
