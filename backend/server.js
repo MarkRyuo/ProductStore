8ed4689dea8a28f65671aeb898c1011cfa20262d
MarkRyuo <malupajhonmark3035@gmail.com>
2025-04-19 11:23:49 +0800

    Subject: Update

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,12 @@ import express from 'express';
 
 const app = express(); //* call express function to create an instance of express
 
-app.listen(5000, (req, res) => { 
-    console.log('Server started at http://localhost:5000');
+app.get("/", (req, res) => { 
+  res.send("Server is Ready");
+})
+
+app.listen(5000, () => { 
+    console.log('Server started at http://localhost:5000 hello');
 })
 
 
