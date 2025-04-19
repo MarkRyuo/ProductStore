f886db518eab5ade5f2fcd892631079843a3a916
MarkRyuo <malupajhonmark3035@gmail.com>
2025-04-19 12:30:54 +0800

    Subject: Update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,9 @@
+
 import mongoose from 'mongoose'; //* Basically going to be running to able to connect to the database by using the connection string (process.env.MONGO_URI) and the mongoose library
 
 export const connectDB = async () => { 
   try {
-    const mongoose = require('mongoose');
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) { 
     console.error(`Error: ${error.message}`);
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,15 @@
 // ! Entry point of our server
 
 import express from 'express';
+import { connectDB } from './config/db.js'; //* import the connectDB function from the db.js file
+import dotenv from 'dotenv'; //* import dotenv to load environment variables from .env file
 
 const app = express(); //* call express function to create an instance of express
 dotenv.config(); //* load environment variables from .env file
 
-app.get("/products", (req, res) => { 
-  res.send("Server is Ready");
-})
+app.get("/products", (req, res) => {})
 
-app.listen(5000, () => { 
+app.listen(5000, () => {
+    connectDB(); //* connect to the database
     console.log('Server started at http://localhost:5000');
 })
-
-//* Ze8AFppuTRUOf8MG
-
-console.log(process.env.MONGO_URI);
