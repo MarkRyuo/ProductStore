989dc0369692329ccc69f63f73289893bed9020d
MarkRyuo <malupajhonmark3035@gmail.com>
2025-04-21 21:34:10 +0800

    Subject: Update

--- backend/server.js
+++ backend/server.js
@@ -44,3 +44,5 @@ app.listen(5000, () => {
 //? get, post, put, delete - is a set of rules and protocols for building and interacting with software applications. It defines the methods and data formats that applications can use to communicate with each other.
 
 
+
+
